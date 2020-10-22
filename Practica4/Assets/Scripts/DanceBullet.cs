0f664f24e2c149eccd8299ce5835feb92f01a05c
LeonardoTJ <leonardo.melendez@uabc.edu.mx>
2020-10-21 19:34:56 -0700

    Agregar balas y script PickupObject

---
+++ Practica4/Assets/Scripts/DanceBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceBullet : Bullet
+{
+    protected override void Start()
+    {
+        base.Start();
+        bullet = GameObject.Find("Dance Bullet");
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        Debug.Log("Dance");
+    }
+
+    protected override void EnableInteract()
+    {
+        base.EnableInteract();
+        MessageText[0].text = "<Dance> Bullet";
+    }
+}
---
+++ Practica4/Assets/Scripts/DetectBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectBullet : Bullet
+{
+    protected override void Start()
+    {
+        base.Start();
+        bullet = GameObject.Find("Detect Bullet");
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        Debug.Log("Detect");
+    }
+
+    protected override void EnableInteract()
+    {
+        base.EnableInteract();
+        MessageText[0].text = "<Detect> Bullet";
+    }
+}
