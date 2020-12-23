28df996408af46fe578d003943beaeea6d2df7c5
LeonardoTJ <leonardo.melendez@uabc.edu.mx>
2020-12-22 22:33:01 -0800

    Commit inicial practica 10

---
+++ Practica10/Assets/Scripts/MotionSensor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using UnityEngine.UI;
+using System;
+
+public class MotionSensor : MonoBehaviour
+{
+    public string brokerIpAddress = "localhost";
+	public int brokerPort = 1883;
+	public string motionTopic = "casa/patio/movimiento";
+    private MqttClient client;
+    string lastMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        client = new MqttClient(brokerIpAddress, brokerPort, false, null);
+        string clientId = Guid.NewGuid().ToString(); 
+		client.Connect(clientId); 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entro");
+        if(other.CompareTag("Player"))
+        {
+            client.Publish(motionTopic,
+                           System.Text.Encoding.UTF8.GetBytes("enter"), 
+                           MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, 
+                           true);
+        }
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Salio");
+        if(other.CompareTag("Player"))
+        {
+            client.Publish(motionTopic,
+                           System.Text.Encoding.UTF8.GetBytes("exit"), 
+                           MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, 
+                           true);
+        }
+    }
+
+    
+    void OnApplicationQuit()
+	{
+		client.Disconnect();
+	}
+
+}
